d7fee2497fecb416f4c319ca25445933066c3509
Ro Rudra <rr1960@nyu.edu>
2016-06-22 10:02:42 -0400

    added another if statement

--- workshop.rb
+++ workshop.rb
@@ -3,6 +3,8 @@
 def factorial(num)
 	if num ==0
 		return 1
+	elsif num < 0
+		return "This is undefined!"
 	else 
 		return num * factorial(num-1)
 	end
@@ -18,15 +20,16 @@ def rotate(array,index)
 		a=array
 		b=array.shift(index)
 		array=(a<<b).flatten!
+	elsif index > array.length
+		return "This is invalid"
 	else
 		a=array
 		b=array.shift(index)
 		array=(a<<b).flatten!
+	end
 end
 
 print rotate([0,1,2,3,4,5,6],2)
-print rotate(["cat", "dog", "mouse", "shoe"], 2)
+print rotate(["cat", "dog", "mouse", "shoe"], 3)
 
 
-if index < 0
-	array[-1]=array[0]
